eb0073b8830f69304d6aca219ebdc8a9427b156c
szyszak <krzszyszko@gmail.com>
2022-05-23 23:03:56 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ScreenSaverOption } from 'ngx-screen-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'screen-saver-package-test';
+export class AppComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
+
+  variants = [
+    {
+      name: 'fireworks',
+      value: 'fireworks',
+    },
+    {
+      name: 'DVD',
+      value: 'dvd',
+    },
+    {
+      name: 'stars',
+      value: 'stars',
+    },
+  ];
+
+  variant: ScreenSaverOption = 'fireworks';
+  idleAfterMs: number = 10000;
+  opacity: number = 1;
+  zIndex: number = 1;
+
+  renderScreenSaverComponent?: boolean;
+
+  defaultValues = {
+    variant: 'fireworks',
+    idleAfterMs: 10000,
+    opacity: 1,
+    zIndex: 1,
+  };
+
+  form = this.fb.group({
+    variant: 'fireworks',
+    idleAfterMs: 10000,
+    opacity: 1,
+    zIndex: 1,
+  });
+
+  onSubmitButtonClick() {
+    // update lokalnych wartosci do tych z formularza i rerender komponentu
+    console.log('submitting');
+
+    const { variant, idleAfterMs, opacity, zIndex } = this.form.value;
+
+    this.variant = variant;
+    this.idleAfterMs = idleAfterMs;
+    this.opacity = opacity;
+    this.zIndex = zIndex;
+
+    this.rerenderScreenSaverComponent();
+  }
+
+  onResetButtonClick(event: Event) {
+    // reset formularza do initialState i rerender komponentu
+    event.preventDefault();
+
+    this.form.reset(this.defaultValues);
+
+    this.rerenderScreenSaverComponent();
+  }
+
+  rerenderScreenSaverComponent() {
+    // tutaj magia ktora niszczy i tworzy na nowo <ngx-screen-saver></ngx-screen-saver>
+    this.renderScreenSaverComponent = false;
+    this.renderScreenSaverComponent = true;
+  }
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe((formValue) => {
+      // console.log(formValue);
+
+      this.variant = formValue.variant;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DropdownModule } from 'primeng/dropdown';
+import { NgxScreenSaverModule } from 'ngx-screen-saver';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    NgxScreenSaverModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
